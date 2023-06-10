30785424a913c4a421b0ddc618d5a790e8830631
iagoalvesb <71942285+iagoalvesb@users.noreply.github.com>
2023-06-10 01:56:05 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import json
+from flask import request
+from flask import Flask, render_template
+
+import os
+from langchain import PromptTemplate, LLMChain
+from langchain.chat_models import ChatOpenAI
+
+
+TEMPLATE = """  Você vai agir como um psicólogo que observa textos de alunos e consegue extrair informações sobre o sentimento do aluno, o
+                real sentimento dele quando estava escrevendo o texto.
+                As características que você irá relatar no texto são [positivo/negativo/neutro], [com/sem violência], [nível de preocupação baixo/médio/alto].
+                Suas respostas devem sempre separar as características por virgula, jamais misture duas características sem a separação delas por
+                vírgula. Lembre-se de sempre verificar se o texto possui um sentimento negativo, se relata alguma violência (física ou verbal), e
+                você sabe que bullying também é violência.
+                
+                Comece!
+
+                Texto: "hoje nao teve nada demais"
+                Psicólogo: neutro, sem violência, baixo.
+
+                Texto: "hoje eu sofri bullying, fizeram muitas piadas sobre mim"
+                Psicólogo: negativo, com violência, médio.
+
+                Texto: {student_text}
+                Psicólogo: 
+ """
+
+
+os.environ['OPENAI_API_KEY'] = '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'
+llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
+
+prompt = PromptTemplate(
+    template=TEMPLATE,
+    input_variables=['student_text']
+)
+
+llm_chain = LLMChain(
+    prompt=prompt,
+    llm=llm
+)
+
+def classify_text(student_text):
+    return llm_chain.run(student_text)
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST'])
+def get_api_response():
+    output = request.get_json()
+    dict_values = json.loads(output)
+    dict_values['texto'] = classify_text(dict_values['texto'])
+    return json.dumps(dict_values)
+
+if __name__ == "__main__":
+    app.run()
